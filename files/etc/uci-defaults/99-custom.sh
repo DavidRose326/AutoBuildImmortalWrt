49520c00daff4c16079264c3d2fc55933a9e6b23
DavidRose326 <106008326@proton.me>
2025-01-08 02:52:52 +0800

    99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -20,11 +20,12 @@ for iface in /sys/class/net/*; do
 done
 
 # 网络设置
-if [ "$count" -eq 1 ]; then
-  uci set network.lan.proto='dhcp'
-elif [ "$count" -gt 1 ]; then
-  uci set network.lan.ipaddr='192.168.1.1'
-fi
+# if [ "$count" -eq 1 ]; then
+#  uci set network.lan.proto='dhcp'
+# elif [ "$count" -gt 1 ]; then
+#  uci set network.lan.ipaddr='192.168.1.1'
+# fi
+uci set network.lan.ipaddr='192.168.1.2'
 
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
@@ -35,7 +36,7 @@ uci commit
 
 # 设置编译作者信息
 FILE_PATH="/etc/openwrt_release"
-NEW_DESCRIPTION="Compiled by wukongdaily"
+NEW_DESCRIPTION="Compiled by hb2004"
 sed -i "s/DISTRIB_DESCRIPTION='[^']*'/DISTRIB_DESCRIPTION='$NEW_DESCRIPTION'/" "$FILE_PATH"
 
 exit 0
