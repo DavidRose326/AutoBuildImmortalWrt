ff210a4f63109fff58acd5b64d7c7fa87907e814
DavidRose326 <106008326@proton.me>
2025-02-26 02:28:54 +0800

    Update 99-custom.sh

--- files/etc/uci-defaults/99-custom.sh
+++ files/etc/uci-defaults/99-custom.sh
@@ -45,7 +45,7 @@ uci set dhcp.lan.ra='server'
 uci set dhcp.lan.dhcpv6='server'
 
 # 设置所有网口可访问网页终端
-# uci delete ttyd.@ttyd[0].interface
+uci delete ttyd.@ttyd[0].interface
 
 # 设置所有网口可连接 SSH
 uci set dropbear.@dropbear[0].Interface=''
